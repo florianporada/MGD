bcf82545c48990c8c35f983ecd6715daafe5f80d
florian porada <florian.porada@gmail.com>
2015-08-25 15:15:24 +0200

    Game.java

---
+++ src/de/hdmstuttgart/mi7/mgd/graphics/GraphicsDevice.java
@@ -0,0 +1,24 @@
+package de.hdmstuttgart.mi7.mgd.graphics;
+
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by florianporada on 25.08.15.
+ */
+public class GraphicsDevice {
+    public void onSurfaceCreated(GL10 gl){
+
+    }
+
+    public void clear(float red, float green, float blue, float alpha){
+
+    }
+
+    public void clear(float red, float green, float blue){
+
+    }
+
+    public void resize(int width, int height){
+
+    }
+}
